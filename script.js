1ac498f0fbe5cde3e78defbd01ba3d65e71fa966
Jordan Quinn <jpquinn605@gmail.com>
2017-10-09 19:30:46 -0600

    Minor syntax fixes - added required semi colons, spaces, etc

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ var winner ;
 var userGuess ;
 var min ;
 var max ;
-var userMessage = document.getElementById('userMessage')
-var minNumber = document.getElementById('minRange')
-var maxNumber = document.getElementById('maxRange')
-var submitRange = document.getElementById('rangeButton')
+var userMessage = document.getElementById('userMessage') ;
+var minNumber = document.getElementById('minRange') ;
+var maxNumber = document.getElementById('maxRange') ; 
+var submitRange = document.getElementById('rangeButton') ;
 
-clear.disabled = true;
-reset.disabled = true;
+clear.disabled = true ;
+reset.disabled = true ;
 
 function rangeSet() {
   
   min = Math.ceil(minNumber.value) ;
   max = Math.floor(maxNumber.value) ;
   if(min > max || minNumber.value.length === 0 || maxNumber.value.length === 0){
-    userMessage.innerText = ('Please enter a valid range')
+    userMessage.innerText = ('Please enter a valid range') ;
   }
   else{
-  winner = Math.floor(Math.random() * (max - min)) + min;
-  console.log('Winning number is ' + winner)
-  console.log('Min num is: ' + min + ' Max num is: ' +  max)
-  userMessage.innerText = ('Number is set between ' + min + ' and ' + max + ', begin guessing!')
+  winner = Math.floor(Math.random() * (max - min)) + min ;
+  console.log('Winning number is ' + winner) ;
+  console.log('Min num is: ' + min + ' Max num is: ' +  max) ;
+  userMessage.innerText = ('Number is set between ' + min + ' and ' + max + ', begin guessing!') ;
   reset.disabled = false ;
 
 }
@@ -44,8 +44,8 @@ function clearEnable() {
 function guessGrabber() {
 
   currentGuess.innerText = input.value ;
-  userGuess = parseInt(input.value, 10)
-  console.log('Current guess is: ' + userGuess);
+  userGuess = parseInt(input.value, 10) ;
+  console.log('Current guess is: ' + userGuess) ;
 
 }
 
@@ -80,7 +80,7 @@ function winTester() {
   }
   else if(userGuess === winner) {
     rangeAdder();
-    console.log('Minimum number: ' + min + ' Maximum number: ' + max)
+    console.log('Minimum number: ' + min + ' Maximum number: ' + max) ;
   }
   else if(userGuess > winner) {
     userMessage.innerText = 'That guess is too high.' ;
@@ -94,17 +94,17 @@ function rangeAdder() {
 
     min -= 10;
     max += 10;
-    userMessage.innerText = ('GREAT JOB! Let\'s make it harder...\n The number to guess is now between ' + min + ' and ' + max)
-    winner = Math.floor(Math.random() * (max - min)) + min
-    console.log('The new winning number is: ' + winner)
-    minNumber.value = min
-    maxNumber.value = max
+    userMessage.innerText = ('GREAT JOB! Let\'s make it harder...\n The number to guess is now between ' + min + ' and ' + max) ;
+    winner = Math.floor(Math.random() * (max - min)) + min ;
+    console.log('The new winning number is: ' + winner) ;
+    minNumber.value = min ;
+    maxNumber.value = max ;
 
   }
 
-input.addEventListener('input', clearEnable)
-submitRange.addEventListener('click', rangeSet)
-guess.addEventListener('click', guessGrabber)
-guess.addEventListener('click', winTester)
-clear.addEventListener('click', clearInput) 
-reset.addEventListener('click', resetScreen)
+input.addEventListener('input', clearEnable) ;
+submitRange.addEventListener('click', rangeSet) ;
+guess.addEventListener('click', guessGrabber) ;
+guess.addEventListener('click', winTester) ; 
+clear.addEventListener('click', clearInput) ;
+reset.addEventListener('click', resetScreen) ;
